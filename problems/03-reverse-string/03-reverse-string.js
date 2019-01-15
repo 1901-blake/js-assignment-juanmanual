10cbba627cb127fa1afd24dddaf59fc287d610ed
juan0melendez <jmelendez2+code@cub.uca.edu>
2019-01-15 18:44:29 -0500

    completed problem 3

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,10 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+  reversedArray = someStr.split("")
+  result = ""
+  while(reversedArray.length > 0){
+    result = result + reversedArray.pop() 
+  }
+  return result
 }
