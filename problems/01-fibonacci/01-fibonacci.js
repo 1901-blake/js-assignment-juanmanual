ffec485564f9548c7348416994a56299fecce845
juan0melendez <jmelendez2+code@cub.uca.edu>
2019-01-15 11:11:44 -0500

    finished fibonacci

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,16 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
+    let current = 1;
+    let last = 1;
 
+    let swap;
+    if(n<3) return 1;
+    for(let i = 1; i<n; i++)
+    {
+       swap = current;
+       current = current + last;
+       last = swap;
+    }
+    return last;
 }
