2eeb4df392a937c881e41b4eef1e718955c548fa
juan0melendez <jmelendez2+code@cub.uca.edu>
2019-01-15 20:43:12 -0500

    finished problem 5

--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -4,5 +4,18 @@ Return the substring contained between startIndex (inclusive) and endIndex (excl
 If incorrect input is entered, throw an exception stating the input was incorrect.
 For example substring('hello', 0, 3) should give 'hel' */
 function substring(someStr, startIndex, endIndex) {
-
+  let result = ""
+  if (typeof someStr !== "string")
+    throw TypeError(`first parameter is type ${typeof someStr} but expected type string`)
+  if (startIndex < 0)
+    return
+  if (startIndex > endIndex)
+    return
+  if (endIndex > someStr.length)
+    return
+  for(let i = startIndex; i < endIndex; i ++)
+    result += someStr.charAt(i);
+  return result
 }
+
+console.log(substring('hello', 0, 3))
