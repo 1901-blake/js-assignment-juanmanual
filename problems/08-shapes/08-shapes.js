690efd9941838379c96853ffb389573821b73e09
juan0melendez <jmelendez2+code@cub.uca.edu>
2019-01-15 20:25:43 -0500

    got the subfunctions to work

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -20,6 +20,49 @@ Example for printShape("Diamond", 5, "*");
  ***
   * 
 */
-function printShape(shape, height, character) {
+function printShape (shape, height, character) {
   
 }
+
+function printSquare (height, character) {
+  let line = "";
+  for (let i = 0; i < height; i++) {
+    line += character
+  }
+  for (let i = 0; i < height; i++) {
+    console.log(line)
+  }
+
+}
+
+function printDiamond (height, character) {
+  let line = ""
+  // build the triangle up
+  for(let i = 1; i<height; i+=2)
+    console.log(" ".repeat((height-i)/2) + character.repeat(i))
+
+  // middle layer
+  console.log(character.repeat(height))
+
+  // going down!
+  for(let i = height-2; i>0; i-=2)
+    console.log(" ".repeat((height-i)/2) + character.repeat(i))
+}
+
+function printTriangle (height, character) {
+let line = "";
+  for (let i = 0; i < height; i++) {
+    line += character
+    console.log(line)
+  }
+}
+
+printDiamond(1, "*")
+printDiamond(3, "*")
+printDiamond(11, "*")
+printTriangle(1, "*")
+printTriangle(3, "*")
+printTriangle(5, "*")
+printSquare(1,"*")
+printSquare(3,"*")
+printSquare(5,"*")
