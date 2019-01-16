6d177487f5f4c9c90143684bfd7200f7b0c3decf
juan0melendez <jmelendez2+code@cub.uca.edu>
2019-01-15 19:20:16 -0500

    finished problem 7

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,10 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+    palArray = someStr.split("")
+
+    while(palArray.length >= 2) {
+      if (palArray.pop() !== palArray.shift()) return false
+    }
+    return true
 }
